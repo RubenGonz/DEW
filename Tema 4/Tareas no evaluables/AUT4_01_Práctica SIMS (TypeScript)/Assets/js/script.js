4cf3fd2a4970c968ede16eb10ed68e58fbbdcf90
RubenGonz <ruben30303030@gmail.com>
2022-01-14 23:48:14 +0000

    Ejercicio de los SIMS documentado con la implementacion de la hora actual

--- Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/js/script.js
+++ Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/js/script.js
@@ -1,15 +1,34 @@
+/**
+ * Clase habitacion que contiene el nombre de
+ * una habitacion y sus posibles frases
+ */
 var habitacion = /** @class */ (function () {
+    /**
+     * Constructor de una habitacion
+     * @param nombre nombre de la habitacion
+     * @param posiblesFrases array de posibles frases relacionadas
+     */
     function habitacion(nombre, posiblesFrases) {
         this.nombre = nombre;
         this.posiblesFrases = posiblesFrases;
     }
+    /**
+     * Funcion que escoge aleatoriamente una de las posibles frases
+     * @returns
+     */
     habitacion.prototype.escogerFrase = function () {
         var indiceRandom = Math.floor(Math.random() * this.posiblesFrases.length);
         return this.posiblesFrases[indiceRandom];
     };
     return habitacion;
 }());
+/**
+ * Constante que recoge todos los nombres del programa
+ */
 var nombres = ["Carlos", "Gonzalo", "Luisa", "Fernando", "Arnaldo", "Carolina"];
+/**
+ * Constante que recoge todas las habitaciones del programa
+ */
 var habitaciones = {
     "Salon": new habitacion("Salon", [
         "está viendo la tele en el salón",
@@ -36,6 +55,9 @@ var habitaciones = {
         "está escuchando musica en el dormitorio"
     ])
 };
+/**
+ * Constante DOM que recoge algunos elementos del html
+ */
 var DOM = {
     contenedorOpciones: document.getElementById("contenedorOpciones"),
     nombres: document.getElementById("nombres"),
@@ -43,9 +65,19 @@ var DOM = {
     oraciones: document.getElementById("oraciones"),
     limpiar: document.getElementById("limpiar")
 };
+/**
+ * Funcion que elimina el contenido de un contenedor
+ * @param id id del contenedor
+ */
 function eliminarContenido(id) {
     document.getElementById(id).innerHTML = "";
 }
+/**
+ * Funcion que agrega una nueva opcion al contenedor de los
+ * nombres o de las habitaciones
+ * @param idPadre id del contenedor padre
+ * @param contenido texto del contenedor
+ */
 function agregarOpcion(idPadre, contenido) {
     var contenedorPadre = document.getElementById(idPadre);
     var input = document.createElement("input");
@@ -57,33 +89,65 @@ function agregarOpcion(idPadre, contenido) {
     var label = document.createElement("label");
     if (idPadre == "nombres")
         label.classList.add("btn", "btn-outline-primary", "p-1");
-    else
+    else if (idPadre == "habitaciones")
         label.classList.add("btn", "btn-outline-success", "p-0");
     label.htmlFor = contenido;
     label.innerHTML = contenido;
     contenedorPadre.appendChild(label);
 }
+/**
+ * Funcion que muestra las opciones
+ * de los nombres
+ */
 function mostrarNombres() {
     eliminarContenido("nombres");
     nombres.forEach(function (nombre) {
         agregarOpcion("nombres", nombre);
     });
 }
+/**
+ * Funcion que muestra las opciones
+ * de las habitaciones
+ */
 function mostrarHabitaciones() {
     eliminarContenido("habitaciones");
     Object.keys(habitaciones).map(function (habitacion) {
         agregarOpcion("habitaciones", habitaciones[habitacion].nombre);
     });
 }
+/**
+ * Funcion que recoge la hora y los minutos actuales
+ * @returns hora en formato hora:minutos
+ */
+function horaActual() {
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    if (hora < 10)
+        hora = 0 + hora;
+    if (minutos < 10)
+        minutos = 0 + minutos;
+    return hora + ":" + minutos;
+}
+/**
+ * Funcion que crea y muestra una oracion nueva
+ * @param nombrePersona nombre de la persona de la nueva oracion
+ * @param nombreHabitacion nombre de la habitacion de la nueva oracion
+ */
 function mostrarNuevaOracion(nombrePersona, nombreHabitacion) {
     var oracionCreada;
-    oracionCreada = nombrePersona + " " + habitaciones[nombreHabitacion].escogerFrase();
+    oracionCreada = nombrePersona + " " + habitaciones[nombreHabitacion].escogerFrase() + " desde las " + horaActual() + ".";
     var contenedor = document.createElement("div");
     contenedor.innerHTML = oracionCreada;
     if (DOM.oraciones.querySelector("#oracionPredeterminada") != undefined)
         eliminarContenido("oraciones");
     DOM.oraciones.appendChild(contenedor);
 }
+/**
+ * Evento que intenta crear una
+ * nueva oracion si ambas opciones
+ * estan marcadas
+ */
 DOM.contenedorOpciones.addEventListener("click", function (e) {
     var target = e.target;
     if (target.nodeName == "INPUT") {
@@ -94,6 +158,9 @@ DOM.contenedorOpciones.addEventListener("click", function (e) {
         }
     }
 });
+/**
+ * Evento que elimina las oraciones y muestra un mensaje
+ */
 DOM.limpiar.addEventListener("click", function () {
     eliminarContenido("oraciones");
     var contenedorPredeterminado = document.createElement("div");
@@ -102,6 +169,9 @@ DOM.limpiar.addEventListener("click", function () {
     contenedorPredeterminado.innerHTML = "Aquí se mostraran las oraciones";
     DOM.oraciones.appendChild(contenedorPredeterminado);
 });
+/**
+ * Acciones a ejecutar cuando cargue la pagina
+ */
 window.onload = function () {
     mostrarNombres();
     mostrarHabitaciones();
--- Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/ts/script.ts
+++ Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/ts/script.ts
@@ -1,26 +1,50 @@
+/**
+ * Interfaz que tiene la informacion 
+ * que debe recoger una habitacion
+ */
 interface interfazHabitacion {
     nombre: string;
     posiblesFrases: string[];
     escogerFrase(): string;
 }
 
+/**
+ * Clase habitacion que contiene el nombre de 
+ * una habitacion y sus posibles frases
+ */
 class habitacion implements interfazHabitacion {
+    //Variables
     nombre: string;
     posiblesFrases: string[];
 
+    /**
+     * Constructor de una habitacion
+     * @param nombre nombre de la habitacion
+     * @param posiblesFrases array de posibles frases relacionadas
+     */
     constructor(nombre: string, posiblesFrases: string[]) {
         this.nombre = nombre;
         this.posiblesFrases = posiblesFrases;
     }
 
+    /**
+     * Funcion que escoge aleatoriamente una de las posibles frases
+     * @returns 
+     */
     escogerFrase(): string {
         let indiceRandom = Math.floor(Math.random() * this.posiblesFrases.length)
         return this.posiblesFrases[indiceRandom];
     }
 }
 
+/**
+ * Constante que recoge todos los nombres del programa
+ */
 const nombres = ["Carlos", "Gonzalo", "Luisa", "Fernando", "Arnaldo", "Carolina"];
 
+/**
+ * Constante que recoge todas las habitaciones del programa
+ */
 const habitaciones = {
     "Salon": new habitacion("Salon", [
         "está viendo la tele en el salón",
@@ -48,6 +72,9 @@ const habitaciones = {
     ])
 }
 
+/**
+ * Constante DOM que recoge algunos elementos del html
+ */
 const DOM = {
     contenedorOpciones: document.getElementById("contenedorOpciones"),
     nombres: document.getElementById("nombres"),
@@ -56,10 +83,20 @@ const DOM = {
     limpiar: document.getElementById("limpiar")
 }
 
+/**
+ * Funcion que elimina el contenido de un contenedor
+ * @param id id del contenedor
+ */
 function eliminarContenido(id: string): void {
     document.getElementById(id).innerHTML = "";
 }
 
+/**
+ * Funcion que agrega una nueva opcion al contenedor de los 
+ * nombres o de las habitaciones
+ * @param idPadre id del contenedor padre
+ * @param contenido texto del contenedor
+ */
 function agregarOpcion(idPadre: string, contenido: string): void {
     let contenedorPadre = document.getElementById(idPadre);
 
@@ -72,12 +109,16 @@ function agregarOpcion(idPadre: string, contenido: string): void {
 
     let label = document.createElement("label");
     if (idPadre == "nombres") label.classList.add("btn", "btn-outline-primary", "p-1");
-    else label.classList.add("btn", "btn-outline-success", "p-0");
+    else if (idPadre == "habitaciones") label.classList.add("btn", "btn-outline-success", "p-0");
     label.htmlFor = contenido;
     label.innerHTML = contenido;
     contenedorPadre.appendChild(label);
 }
 
+/**
+ * Funcion que muestra las opciones 
+ * de los nombres
+ */
 function mostrarNombres(): void {
     eliminarContenido("nombres");
     nombres.forEach(nombre => {
@@ -85,6 +126,10 @@ function mostrarNombres(): void {
     })
 }
 
+/**
+ * Funcion que muestra las opciones 
+ * de las habitaciones
+ */
 function mostrarHabitaciones(): void {
     eliminarContenido("habitaciones");
     Object.keys(habitaciones).map(habitacion => {
@@ -92,15 +137,38 @@ function mostrarHabitaciones(): void {
     })
 }
 
+/**
+ * Funcion que recoge la hora y los minutos actuales
+ * @returns hora en formato hora:minutos
+ */
+function horaActual(): string {
+    let fecha = new Date();
+    let hora = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    if (hora < 10) hora = 0 + hora;
+    if (minutos < 10) minutos = 0 + minutos;
+    return hora + ":" + minutos
+}
+
+/**
+ * Funcion que crea y muestra una oracion nueva
+ * @param nombrePersona nombre de la persona de la nueva oracion
+ * @param nombreHabitacion nombre de la habitacion de la nueva oracion
+ */
 function mostrarNuevaOracion(nombrePersona: string, nombreHabitacion: string): void {
     let oracionCreada: string;
-    oracionCreada = nombrePersona + " " + habitaciones[nombreHabitacion].escogerFrase();
+    oracionCreada = nombrePersona + " " + habitaciones[nombreHabitacion].escogerFrase() + " desde las " + horaActual() + ".";
     let contenedor = document.createElement("div");
     contenedor.innerHTML = oracionCreada;
     if (DOM.oraciones.querySelector("#oracionPredeterminada") != undefined) eliminarContenido("oraciones")
     DOM.oraciones.appendChild(contenedor);
 }
 
+/**
+ * Evento que intenta crear una 
+ * nueva oracion si ambas opciones 
+ * estan marcadas
+ */
 DOM.contenedorOpciones.addEventListener("click", (e: MouseEvent): void => {
     let target = e.target as Element;
     if (target.nodeName == "INPUT") {
@@ -112,6 +180,9 @@ DOM.contenedorOpciones.addEventListener("click", (e: MouseEvent): void => {
     }
 })
 
+/**
+ * Evento que elimina las oraciones y muestra un mensaje
+ */
 DOM.limpiar.addEventListener("click", (): void => {
     eliminarContenido("oraciones");
     let contenedorPredeterminado = document.createElement("div");
@@ -121,6 +192,9 @@ DOM.limpiar.addEventListener("click", (): void => {
     DOM.oraciones.appendChild(contenedorPredeterminado);
 })
 
+/**
+ * Acciones a ejecutar cuando cargue la pagina
+ */
 window.onload = () => {
     mostrarNombres();
     mostrarHabitaciones();
