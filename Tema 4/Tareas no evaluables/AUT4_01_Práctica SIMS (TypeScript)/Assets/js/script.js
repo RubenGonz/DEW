3db94a05826026af67f007803594247517ed3d68
RubenGonz <ruben30303030@gmail.com>
2022-01-14 23:28:38 +0000

    Ejercicio de los sims casi terminada

--- Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/js/script.js
+++ Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/js/script.js
@@ -10,19 +10,99 @@ var habitacion = /** @class */ (function () {
     return habitacion;
 }());
 var nombres = ["Carlos", "Gonzalo", "Luisa", "Fernando", "Arnaldo", "Carolina"];
-var habitaciones = [
-    new habitacion("Salon", ["xiasoj", "gyuguy"]),
-    new habitacion("Cocina", ["xiasoj", "gyuguy"]),
-    new habitacion("Banio", ["xiasoj", "gyuguy"]),
-    new habitacion("Dormitorio", ["xiasoj", "gyuguy"])
-];
+var habitaciones = {
+    "Salon": new habitacion("Salon", [
+        "está viendo la tele en el salón",
+        "está leyendo en el salón",
+        "está con su hermana en el salón",
+        "está descansando en el salón"
+    ]),
+    "Cocina": new habitacion("Cocina", [
+        "está cocinando en la cocina",
+        "está comiendo en la cocina",
+        "está haciendo un bizcocho en la cocina",
+        "está fregando en la cocina"
+    ]),
+    "Baño": new habitacion("Baño", [
+        "se está lavando los dientes en el baño",
+        "se encuentra en la ducha del baño",
+        "está haciendo sus necesidades en el baño",
+        "está limpiando el baño"
+    ]),
+    "Dormitorio": new habitacion("Dormitorio", [
+        "está durmiendo en el dormitorio",
+        "está haciendo tarea en el dormitorio",
+        "está vistiendose en el dormitorio",
+        "está escuchando musica en el dormitorio"
+    ])
+};
+var DOM = {
+    contenedorOpciones: document.getElementById("contenedorOpciones"),
+    nombres: document.getElementById("nombres"),
+    habitaciones: document.getElementById("habitaciones"),
+    oraciones: document.getElementById("oraciones"),
+    limpiar: document.getElementById("limpiar")
+};
+function eliminarContenido(id) {
+    document.getElementById(id).innerHTML = "";
+}
+function agregarOpcion(idPadre, contenido) {
+    var contenedorPadre = document.getElementById(idPadre);
+    var input = document.createElement("input");
+    input.type = "radio";
+    input.classList.add("btn-check");
+    input.name = idPadre;
+    input.id = contenido;
+    contenedorPadre.appendChild(input);
+    var label = document.createElement("label");
+    if (idPadre == "nombres")
+        label.classList.add("btn", "btn-outline-primary", "p-1");
+    else
+        label.classList.add("btn", "btn-outline-success", "p-0");
+    label.htmlFor = contenido;
+    label.innerHTML = contenido;
+    contenedorPadre.appendChild(label);
+}
 function mostrarNombres() {
-    //chechbox
+    eliminarContenido("nombres");
+    nombres.forEach(function (nombre) {
+        agregarOpcion("nombres", nombre);
+    });
 }
 function mostrarHabitaciones() {
-    //chechbox
+    eliminarContenido("habitaciones");
+    Object.keys(habitaciones).map(function (habitacion) {
+        agregarOpcion("habitaciones", habitaciones[habitacion].nombre);
+    });
 }
-function crearOracion(nombrePersona, nombreHabitacion) {
+function mostrarNuevaOracion(nombrePersona, nombreHabitacion) {
     var oracionCreada;
-    return oracionCreada;
+    oracionCreada = nombrePersona + " " + habitaciones[nombreHabitacion].escogerFrase();
+    var contenedor = document.createElement("div");
+    contenedor.innerHTML = oracionCreada;
+    if (DOM.oraciones.querySelector("#oracionPredeterminada") != undefined)
+        eliminarContenido("oraciones");
+    DOM.oraciones.appendChild(contenedor);
 }
+DOM.contenedorOpciones.addEventListener("click", function (e) {
+    var target = e.target;
+    if (target.nodeName == "INPUT") {
+        var nombre = document.querySelectorAll('#nombres input[type="radio"]:checked')[0];
+        var habitacion_1 = document.querySelectorAll('#habitaciones input[type="radio"]:checked')[0];
+        if (nombre != undefined && habitacion_1 != undefined) {
+            mostrarNuevaOracion(nombre.id, habitacion_1.id);
+        }
+    }
+});
+DOM.limpiar.addEventListener("click", function () {
+    eliminarContenido("oraciones");
+    var contenedorPredeterminado = document.createElement("div");
+    contenedorPredeterminado.id = "oracionPredeterminada";
+    contenedorPredeterminado.classList.add("text-center");
+    contenedorPredeterminado.innerHTML = "Aquí se mostraran las oraciones";
+    DOM.oraciones.appendChild(contenedorPredeterminado);
+});
+window.onload = function () {
+    mostrarNombres();
+    mostrarHabitaciones();
+};
--- Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/ts/script.ts
+++ Tema 4/Tareas no evaluables/AUT4_01_Práctica SIMS (TypeScript)/Assets/ts/script.ts
@@ -21,23 +21,107 @@ class habitacion implements interfazHabitacion {
 
 const nombres = ["Carlos", "Gonzalo", "Luisa", "Fernando", "Arnaldo", "Carolina"];
 
-const habitaciones = [
-    new habitacion("Salon",["xiasoj","gyuguy"]),
-    new habitacion("Cocina",["xiasoj","gyuguy"]),
-    new habitacion("Banio",["xiasoj","gyuguy"]),
-    new habitacion("Dormitorio",["xiasoj","gyuguy"])
-]
+const habitaciones = {
+    "Salon": new habitacion("Salon", [
+        "está viendo la tele en el salón",
+        "está leyendo en el salón",
+        "está con su hermana en el salón",
+        "está descansando en el salón"
+    ]),
+    "Cocina": new habitacion("Cocina", [
+        "está cocinando en la cocina",
+        "está comiendo en la cocina",
+        "está haciendo un bizcocho en la cocina",
+        "está fregando en la cocina"
+    ]),
+    "Baño": new habitacion("Baño", [
+        "se está lavando los dientes en el baño",
+        "se encuentra en la ducha del baño",
+        "está haciendo sus necesidades en el baño",
+        "está limpiando el baño"
+    ]),
+    "Dormitorio": new habitacion("Dormitorio", [
+        "está durmiendo en el dormitorio",
+        "está haciendo tarea en el dormitorio",
+        "está vistiendose en el dormitorio",
+        "está escuchando musica en el dormitorio"
+    ])
+}
+
+const DOM = {
+    contenedorOpciones: document.getElementById("contenedorOpciones"),
+    nombres: document.getElementById("nombres"),
+    habitaciones: document.getElementById("habitaciones"),
+    oraciones: document.getElementById("oraciones"),
+    limpiar: document.getElementById("limpiar")
+}
+
+function eliminarContenido(id: string): void {
+    document.getElementById(id).innerHTML = "";
+}
+
+function agregarOpcion(idPadre: string, contenido: string): void {
+    let contenedorPadre = document.getElementById(idPadre);
+
+    let input = document.createElement("input");
+    input.type = "radio";
+    input.classList.add("btn-check");
+    input.name = idPadre;
+    input.id = contenido;
+    contenedorPadre.appendChild(input);
+
+    let label = document.createElement("label");
+    if (idPadre == "nombres") label.classList.add("btn", "btn-outline-primary", "p-1");
+    else label.classList.add("btn", "btn-outline-success", "p-0");
+    label.htmlFor = contenido;
+    label.innerHTML = contenido;
+    contenedorPadre.appendChild(label);
+}
 
 function mostrarNombres(): void {
-    //chechbox
+    eliminarContenido("nombres");
+    nombres.forEach(nombre => {
+        agregarOpcion("nombres", nombre)
+    })
 }
 
 function mostrarHabitaciones(): void {
-    //chechbox
+    eliminarContenido("habitaciones");
+    Object.keys(habitaciones).map(habitacion => {
+        agregarOpcion("habitaciones", habitaciones[habitacion].nombre);
+    })
 }
 
-function crearOracion(nombrePersona: string, nombreHabitacion: string): string {
+function mostrarNuevaOracion(nombrePersona: string, nombreHabitacion: string): void {
     let oracionCreada: string;
+    oracionCreada = nombrePersona + " " + habitaciones[nombreHabitacion].escogerFrase();
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = oracionCreada;
+    if (DOM.oraciones.querySelector("#oracionPredeterminada") != undefined) eliminarContenido("oraciones")
+    DOM.oraciones.appendChild(contenedor);
+}
 
-    return oracionCreada;
+DOM.contenedorOpciones.addEventListener("click", (e: MouseEvent): void => {
+    let target = e.target as Element;
+    if (target.nodeName == "INPUT") {
+        let nombre = document.querySelectorAll('#nombres input[type="radio"]:checked')[0];
+        let habitacion = document.querySelectorAll('#habitaciones input[type="radio"]:checked')[0]
+        if (nombre != undefined && habitacion != undefined) {
+            mostrarNuevaOracion(nombre.id, habitacion.id);
+        }
+    }
+})
+
+DOM.limpiar.addEventListener("click", (): void => {
+    eliminarContenido("oraciones");
+    let contenedorPredeterminado = document.createElement("div");
+    contenedorPredeterminado.id = "oracionPredeterminada";
+    contenedorPredeterminado.classList.add("text-center");
+    contenedorPredeterminado.innerHTML = "Aquí se mostraran las oraciones";
+    DOM.oraciones.appendChild(contenedorPredeterminado);
+})
+
+window.onload = () => {
+    mostrarNombres();
+    mostrarHabitaciones();
 }
