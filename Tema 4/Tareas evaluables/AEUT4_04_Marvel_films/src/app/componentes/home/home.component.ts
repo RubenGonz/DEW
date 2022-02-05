2912e2df96636cf3a235a741e519b20a97b6c998
RubenGonz <ruben30303030@gmail.com>
2022-02-05 14:38:42 +0000

    Modificación de algunos archivos

--- Tema 4/Tareas evaluables/AEUT4_04_Marvel_films/src/app/componentes/home/home.component.ts
+++ Tema 4/Tareas evaluables/AEUT4_04_Marvel_films/src/app/componentes/home/home.component.ts
@@ -7,16 +7,27 @@ import { PeliculaService } from 'src/app/servicios/pelicula.service';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
+
+/**
+ * Componente Home que muestra la página 
+ */
 export class HomeComponent implements OnInit {
 
     peliculasHome: InterfazPelicula[] = [];
 
+    /**
+     * Constructor del componente que inicializa PeliculaService
+     * @param peliculaService servicio que gestiona las peliculas
+     */
     constructor(private peliculaService: PeliculaService) { }
 
     ngOnInit(): void {
         this.getPeliculas();
     }
 
+    /**
+     * Metodo que recoge las peliculas del servicio
+     */
     getPeliculas(): void {
         this.peliculaService.getPeliculas().subscribe(peliculasApi => this.peliculasHome = peliculasApi.slice(0,8));
     }
